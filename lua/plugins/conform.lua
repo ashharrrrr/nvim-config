e00ee5ff5f583a1a757bb2221e5da4edfe617dab
Ashhar Siddiqui <ashhar2002.6@gmail.com>
2025-07-07 00:14:01 +0530

    add keymapping

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -4,6 +4,16 @@ return {
 		formatters_by_ft = {
 			lua = { "stylua" },
 			go = { "goimports", "golines", "gofmt" },
+			javascript = { "prettierd" },
+			typescript = { "prettierd" },
+			javascriptreact = { "prettierd" },
+			typescriptreact = { "prettierd" },
+			json = { "prettierd" },
+			html = { "prettierd" },
+			css = { "prettierd" },
+			scss = { "prettierd" },
+			markdown = { "prettierd" },
+			yaml = { "prettierd" },
 		},
 	},
 	keys = {
